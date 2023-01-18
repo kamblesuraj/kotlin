3f76bcaa66fa3cfa935f68d33ce220e2acb68c36
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-01-18 13:28:49 +0100

    Cleanup unused Kotlin daemon dependencies

--- compiler/daemon/daemon-common/build.gradle.kts
+++ compiler/daemon/daemon-common/build.gradle.kts
@@ -7,9 +7,6 @@ dependencies {
     api(project(":compiler:cli-common"))
     api(project(":kotlin-build-common"))
     api(kotlinStdlib())
-    compileOnly(project(":js:js.config"))
-    compileOnly(intellijCore())
-    api(commonDependency("org.jetbrains.kotlinx", "kotlinx-coroutines-core")) { isTransitive = false }
 }
 
 sourceSets {
