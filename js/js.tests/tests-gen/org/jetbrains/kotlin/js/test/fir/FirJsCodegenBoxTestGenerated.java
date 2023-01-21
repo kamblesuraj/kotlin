f4e3042f895970827d5e165529a1f5d918f983ca
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2023-01-21 15:38:51 +0100

    [FIR & IR] Implement JS MPP test infrastructure
    ^KT-55295 Fixed

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenBoxTestGenerated.java
@@ -23029,6 +23029,12 @@ public class FirJsCodegenBoxTestGenerated extends AbstractFirJsCodegenBoxTest {
             }
 
             @Test
+            @TestMetadata("expectActualSimpleJs.kt")
+            public void testExpectActualSimpleJs() throws Exception {
+                runTest("compiler/testData/codegen/box/multiplatform/multiModule/expectActualSimpleJs.kt");
+            }
+
+            @Test
             @TestMetadata("expectActualTypealiasLink.kt")
             public void testExpectActualTypealiasLink() throws Exception {
                 runTest("compiler/testData/codegen/box/multiplatform/multiModule/expectActualTypealiasLink.kt");
