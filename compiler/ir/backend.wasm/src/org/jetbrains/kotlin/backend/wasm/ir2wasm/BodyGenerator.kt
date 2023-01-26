273077476aa5f103aaac383a1daaed6d6e908111
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-01-27 00:34:45 +0100

    [Wasm] Change `externRefToAny` to make it compatible with SpiderMonkey and V8
    It's hard to achieve that without implementing part of `externRefToAny` using an intrinsic. It should be rewritten when all VMs and tools update to the latest spec.
    
    Also, stop using (deprecated) instructions unsupported by SpiderMonkey.

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/ir2wasm/BodyGenerator.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/ir2wasm/BodyGenerator.kt
@@ -594,11 +594,26 @@ class BodyGenerator(
             }
 
             wasmSymbols.unsafeGetScratchRawMemory -> {
-                
                 body.buildConstI32Symbol(context.scratchMemAddr, location)
             }
 
-            
+            wasmSymbols.returnArgumentIfItIsKotlinAny -> {
+                body.buildBlock("") { _ ->
+                    body.buildBlock("returnIfAny", WasmAnyRef) { innerLabel ->
+                        body.buildGetLocal(functionContext.referenceLocal(0), SourceLocation.NoLocation(""))
+                        body.buildInstr(WasmOp.EXTERN_INTERNALIZE, SourceLocation.NoLocation(""))
+                        body.buildBrInstr(WasmOp.BR_ON_NON_DATA_DEPRECATED, innerLabel, SourceLocation.NoLocation(""))
+                        body.buildBrInstr(
+                            WasmOp.BR_ON_CAST_FAIL_DEPRECATED,
+                            innerLabel,
+                            context.referenceGcType(backendContext.irBuiltIns.anyClass),
+                            SourceLocation.NoLocation("")
+                        )
+                        body.buildInstr(WasmOp.RETURN, SourceLocation.NoLocation(""))
+                    }
+                    body.buildDrop(SourceLocation.NoLocation(""))
+                }
+            }
 
             wasmSymbols.wasmArrayCopy -> {
                 val immediate = WasmImmediate.GcType(
