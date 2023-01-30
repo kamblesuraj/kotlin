b48d00c1f00f48b52368abb8b4d3e59a8f091703
Artem Daugel-Dauge <artem.daugel-dauge@jetbrains.com>
2023-01-30 22:45:31 +0100

    Cleanup

--- libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/targets/native/BlahBlah.kt
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/targets/native/BlahBlah.kt
@@ -5,54 +5,71 @@
 
 package org.jetbrains.kotlin.gradle.targets.native
 
+import org.gradle.api.logging.Logger
+import org.jetbrains.kotlin.gradle.targets.native.PodfileLockParser.LockedPod
 import org.junit.Test
 import org.yaml.snakeyaml.Yaml
 import java.io.File
 
 
-data class Dep(val key: String, val deps: Set<String>) {
-    override fun toString(): String {
-        return deps.joinToString(prefix = "\n$key\n", separator = "\n") { "    - $it" }
+class PodfileLockParser(private val logger: Logger?) {
+
+    data class LockedPod(val key: String, val dependencies: Set<String>)
+
+    private var shouldWarn = false
+
+    fun parse(file: File): List<LockedPod> {
+        shouldWarn = false
+
+        return Yaml().load<List<LockedPod>?>(file.inputStream())
+            .asMap()["PODS"]
+            .asList()
+            .mapNotNull { it.toPod() }
+            .also {
+                if (shouldWarn) {
+                    logger?.warn("w: Unexpected Podfile.lock format")
+                }
+            }
     }
-}
 
-fun Any?.asList(): List<*> = (this as? List<*>) ?: reportUnexpectedFormat() ?: emptyList<Nothing>()
-
-fun reportUnexpectedFormat(): Nothing? {
-    println("Unexpected Podfile.lock format")
-    return null
-}
-
-fun Any?.toDep(): Dep? {
-    return when (this) {
-        is String -> Dep(key = toPodName(), deps = emptySet())
-        is Map<*, *> -> keys.singleOrNull()
-            ?.let { it as? String }
-            ?.let { key ->
-                val list = get(key).asList()
-
-                val deps = list.asSequence()
-                    .mapNotNull { it as? String ?: reportUnexpectedFormat() }
-                    .map { it.toPodName() }
-                    .toSet()
-
-                Dep(key = key.toPodName(), deps = deps)
-            } ?: reportUnexpectedFormat()
-        else -> reportUnexpectedFormat()
+    private fun reportUnexpectedFormat(): Nothing? {
+        shouldWarn = true
+        return null
     }
-}
 
-private fun String.toPodName(): String = substringBefore(' ')
+    private fun Any?.asList(): List<*> = (this as? List<*>) ?: reportUnexpectedFormat() ?: emptyList<Nothing>()
+
+    private fun Any?.asMap(): Map<*, *> = (this as? Map<*, *>) ?: reportUnexpectedFormat() ?: emptyMap<Nothing, Nothing>()
+
+    private fun Any?.toPod(): LockedPod? {
+        return when (this) {
+            is String -> LockedPod(key = toPodName(), dependencies = emptySet())
+            is Map<*, *> -> keys.singleOrNull()
+                ?.let { it as? String }
+                ?.let { key ->
+                    val list = get(key).asList()
+
+                    val deps = list.asSequence()
+                        .mapNotNull { it as? String ?: reportUnexpectedFormat() }
+                        .map { it.toPodName() }
+                        .toSet()
+
+                    LockedPod(key = key.toPodName(), dependencies = deps)
+                } ?: reportUnexpectedFormat()
+            else -> reportUnexpectedFormat()
+        }
+    }
+
+    private fun String.toPodName(): String = substringBefore(' ')
+
+}
 
 
 class BlahBlah {
     @Test
     fun blah() {
-        val lock = Yaml().load<Map<String, Any>>(File("/Users/Artem.Daugel-Dauge/Downloads/xcode-repro/ios-app/Podfile.lock").inputStream())
-            .get("PODS")
-            .asList()
-            .mapNotNull { it.toDep() }
-            .associateBy(Dep::key, Dep::deps)
+        val lock = PodfileLockParser(null).parse(File("/Users/Artem.Daugel-Dauge/Downloads/xcode-repro/ios-app/Podfile.lock"))
+            .associateBy(LockedPod::key, LockedPod::dependencies)
 
         val pods = setOf("AFNetworking", "FirebaseCore", "pod1", "pod2", "SSZipArchive")
 
