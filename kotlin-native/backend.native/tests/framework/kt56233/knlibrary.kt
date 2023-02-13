424e2b3e35294ec4fe310bff471f8f79a5d9bea6
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-02-13 19:32:34 +0100

    Try reproduce on weak references

--- kotlin-native/backend.native/tests/framework/kt56233/knlibrary.kt
+++ kotlin-native/backend.native/tests/framework/kt56233/knlibrary.kt
@@ -2,3 +2,15 @@ enum class SimpleEnum {
     ONE,
     TWO
 }
+
+class X(val num: Int)
+
+private var globalX: X? = null
+
+fun getX(num: Int): X {
+    val x = X(num)
+    globalX = x
+    return x
+}
+
+fun scheduleGC() = kotlin.native.internal.GC.schedule()
