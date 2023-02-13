bb4aaf906dce5aea7b447bba0b06a1195ef6adce
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-02-13 11:29:28 +0100

    !fixup better `copy` in FunctionInlining

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/inline/FunctionInlining.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/inline/FunctionInlining.kt
@@ -176,8 +176,9 @@ class FunctionInlining(
 
         fun inline() = inlineFunction(callSite, callee, inlineFunctionResolver.getFunctionSymbol(callee).owner, true)
 
-        private fun IrElement.copy(): IrElement {
-            return copyIrElement.copy(this)
+        private fun <E : IrElement> E.copy(): E {
+            @Suppress("UNCHECKED_CAST")
+            return copyIrElement.copy(this) as E
         }
 
         private fun inlineFunction(
@@ -186,7 +187,7 @@ class FunctionInlining(
             originalInlinedElement: IrElement,
             performRecursiveInline: Boolean
         ): IrReturnableBlock {
-            val copiedCallee = (callee.copy() as IrFunction).apply {
+            val copiedCallee = callee.copy().apply {
                 parent = callee.parent
                 if (performRecursiveInline) {
                     body?.transformChildrenVoid()
@@ -265,7 +266,7 @@ class FunctionInlining(
                     if (argument is IrGetValueWithoutLocation)
                         argument.withLocation(newExpression.startOffset, newExpression.endOffset)
                     else
-                        (argument.copy() as IrExpression)
+                        argument.copy()
 
                 if (insertAdditionalImplicitCasts)
                     ret = ret.implicitCastIfNeededTo(newExpression.type)
@@ -437,10 +438,10 @@ class FunctionInlining(
                                 val arg = boundFunctionParametersMap[parameter]!!
                                 if (arg is IrGetValueWithoutLocation)
                                     arg.withLocation(irCall.startOffset, irCall.endOffset)
-                                else arg.copy() as IrExpression
+                                else arg.copy()
                             } else {
                                 if (unboundIndex == valueParameters.size && parameter.defaultValue != null)
-                                    parameter.defaultValue!!.expression.copy() as IrExpression
+                                    parameter.defaultValue!!.expression.copy()
                                 else if (!parameter.isVararg) {
                                     assert(unboundIndex < valueParameters.size) {
                                         "Attempt to use unbound parameter outside of the callee's value parameters"
