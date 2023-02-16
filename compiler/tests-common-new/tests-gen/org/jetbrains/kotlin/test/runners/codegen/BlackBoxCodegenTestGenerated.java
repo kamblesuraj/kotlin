557a9d28bd4b6df8c38fa94aee29514d6ff602c4
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-02-16 16:17:32 +0100

    [K2/N] Add failed test for KT-56747

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
@@ -44416,6 +44416,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
         }
 
         @Test
+        @TestMetadata("kt56747.kt")
+        public void testKt56747() throws Exception {
+            runTest("compiler/testData/codegen/box/regressions/kt56747.kt");
+        }
+
+        @Test
         @TestMetadata("kt5786_privateWithDefault.kt")
         public void testKt5786_privateWithDefault() throws Exception {
             runTest("compiler/testData/codegen/box/regressions/kt5786_privateWithDefault.kt");
