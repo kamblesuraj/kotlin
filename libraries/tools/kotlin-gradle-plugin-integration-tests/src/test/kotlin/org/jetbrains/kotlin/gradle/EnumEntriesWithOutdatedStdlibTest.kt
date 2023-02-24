9a5fa5fa6baa9e9d1703f6583f227597a6f7a5f6
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-02-24 15:21:00 +0200

    fixup! KT-56687: Unify handling of `EnumEntries`.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/EnumEntriesWithOutdatedStdlibTest.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/EnumEntriesWithOutdatedStdlibTest.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.gradle
 
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.gradle.util.replaceText
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("EnumEntries feature test when no kotlin.enums.EnumEntries found on the classpath")
@@ -23,6 +24,20 @@ class EnumEntriesWithOutdatedStdlibTest : KGPBaseTest() {
             buildAndFail(":k2:compileKotlin") {
                 assertOutputContains("MainK2.kt:9:31 Unresolved reference: entries")
             }
+        }
+    }
+
+    @DisplayName("Code should compile normally if `entries` is never referenced")
+    @GradleTest
+    fun codeCompilesWithoutReferencingEntries(gradleVersion: GradleVersion) {
+        project("enumEntriesNotAccessible", gradleVersion) {
+            projectPath.resolve("k1/src/main/kotlin/MainK1.kt")
+                .replaceText("println(AnnotationTarget.entries)", "println(\"Hello!\")")
+            build(":k1:compileKotlin")
+
+            projectPath.resolve("k2/src/main/kotlin/MainK2.kt")
+                .replaceText("val it = AnnotationTarget.entries", "println(\"Hello!\")")
+            build(":k2:compileKotlin")
         }
     }
 }
